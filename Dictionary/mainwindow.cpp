8623ad22cb0b4b76ff41175e73b18b9d68dac4b3
Charles Carlson <carlso13@stolaf.edu>
2018-05-18 13:50:41 -0500

    added comments

--- Dictionary/mainwindow.cpp
+++ Dictionary/mainwindow.cpp
@@ -35,14 +35,14 @@
 
 
 
-void MainWindow::search_clicked(){
+void MainWindow::search_clicked(){ // searches for what the user enters into the search bar
     int counter=0;
 
     ui->textBrowser_2->clear();
     ui->textBrowser_3->clear();
 
-    QString searchVal = ui->lineEdit->text();
-    std::string str = searchVal.toStdString();
+    QString searchVal = ui->lineEdit->text();   //text user enters
+    std::string str = searchVal.toStdString();  //converts to string and finds def, then converts to QString
     std::string defn = search_multimap(str);
     std::string similar = search_thes(str);
     QString find_str = QString::fromStdString(str);
@@ -50,23 +50,24 @@ void MainWindow::search_clicked(){
     term << find_str;
     QString find_defn = QString::fromStdString(defn);
 
-    history.push_back(str);
+    history.push_back(str);                     //str is added to vector for history
     history_thes.push_back(similar);
     maxSize++;
 
-    for(int i=0;i<term.length();i++){
+    for(int i=0;i<term.length();i++){           //makes sure term doesnt show up twice in history textbrowser
         if(term[i]==(find_str))
             counter++;
     }
 
     if(counter < 2){
         ui->textBrowser->append(find_str);
-        ui->textBrowser_3->append(find_thes);
+
     }
-    ui->textBrowser_2->append(find_defn);
+    ui->textBrowser_3->append(find_thes);
+    ui->textBrowser_2->append(find_defn);          //appends def and thes entry
     ui->textBrowser_2->textCursor();
     scrollbar.setPosition(0);
-    ui->textBrowser_2->setTextCursor(scrollbar);
+    ui->textBrowser_2->setTextCursor(scrollbar);  //makes sure scrollbar starts at top
 
 
 
@@ -78,16 +79,16 @@ void MainWindow::return_clicked(){
     ui->textBrowser_2->clear();
     ui->textBrowser_3->clear();
 
-    std::string str = history[(maxSize-1) - count];
+    std::string str = history[(maxSize-1) - count]; // goes back to previous entry
     std::string str2 = history_thes[(maxSize-1)-count];
     count++;
-    std::string defn = search_multimap(str);
+    std::string defn = search_multimap(str); //converts string to qstring
     std::string similar = search_thes(str2);
     QString find_defn = QString::fromStdString(defn);
     QString find_str = QString::fromStdString(str);
     QString find_thes = QString::fromStdString(str2);
 
-    ui->textBrowser_2->append(find_defn);
+    ui->textBrowser_2->append(find_defn); //appends previous entry to textbrowser
     ui->textBrowser_3->append(find_thes);
     scrollbar.setPosition(0);
     ui->textBrowser_2->setTextCursor(scrollbar);
@@ -103,19 +104,19 @@ MainWindow::MainWindow(QWidget *parent) :
     if ( homedir.length()== 0) {
         homedir = getpwuid(getuid())->pw_dir;
     }
-    ui->setupUi(this);
+    ui->setupUi(this); //sets up the .ui file
 
 
     std::string m_glass=homedir+"/Dictionary/Dictionary/m_glass2.png";
     QString Qm_glass = QString::fromStdString(m_glass);
-    ui->pushButton->setIcon(QPixmap(Qm_glass));// /home/users/daly2/Dictionary/Dictionary/
+    ui->pushButton->setIcon(QPixmap(Qm_glass));// puts image onto pushButton
 
 
 
     std::string dict_path=homedir+"/Dictionary/Dictionary/dict.txt";
     std::ifstream g(dict_path);
 
-    if(!g){
+    if(!g){                                 //reads in dictionary into QStringList
         std::cerr << "Not found"<<endl;
     }
     std::string title, def;
@@ -131,7 +132,7 @@ MainWindow::MainWindow(QWidget *parent) :
       }
     g.close();
 
-    QCompleter *autocomplete = new QCompleter(titleList,this);
+    QCompleter *autocomplete = new QCompleter(titleList,this); //completer is the suggestion window once user enters in a string, pulls from QStringList
     autocomplete->setWrapAround(false);
     autocomplete->setCaseSensitivity(Qt::CaseInsensitive);
     autocomplete->setModelSorting(QCompleter::CaseInsensitivelySortedModel);
@@ -141,11 +142,11 @@ MainWindow::MainWindow(QWidget *parent) :
 
 
 
-    ui->pushButton->setIconSize(QSize(20,20));
+    ui->pushButton->setIconSize(QSize(20,20)); //makes button size 20,20
 
 
     std::string back_button=homedir+"/Dictionary/Dictionary/firefox_return.png";
-    QString Qback_button = QString::fromStdString(back_button);
+    QString Qback_button = QString::fromStdString(back_button); //puts image on back button
     ui->toolButton->setIcon(QPixmap(Qback_button));
     ui->toolButton->setIconSize(QSize(100,100));
 
@@ -156,7 +157,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->pushButton->setText("Search");
 
 
-    ui->lineEdit->setStyleSheet("border: 1px solid;"
+    ui->lineEdit->setStyleSheet("border: 1px solid;"                    //sets up the textbrowsers
                                 "border-radius:5px;"
                                 "background-color: palette(base);");
     ui->lineEdit->setPlaceholderText("Search...");
@@ -172,9 +173,9 @@ MainWindow::MainWindow(QWidget *parent) :
                                   "background-color: palette(base);");
 
 
-    connect (ui->pushButton, SIGNAL(clicked()),this,SLOT(search_clicked()));
-    connect (ui->toolButton, SIGNAL(clicked()),this,SLOT(return_clicked()));
-    connect(ui->lineEdit,SIGNAL(returnPressed()),ui->pushButton,SIGNAL(clicked()));
+    connect (ui->pushButton, SIGNAL(clicked()),this,SLOT(search_clicked()));//connects the pushbutton to the search_clicked
+    connect (ui->toolButton, SIGNAL(clicked()),this,SLOT(return_clicked()));//connects return button to return_clicked
+    connect(ui->lineEdit,SIGNAL(returnPressed()),ui->pushButton,SIGNAL(clicked())); //allows user to press enter to search rather than press button
 
 }
 
